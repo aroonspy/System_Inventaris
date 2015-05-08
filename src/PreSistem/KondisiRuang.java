b4828a972164667c563b961197026390c2f71a14
khopsawati <hotimahmuaniez@gmail.com>
2015-05-08 18:54:53 +0700

    Sistem Inventaris By Khopsawati khotimah

---
+++ src/PreSistem/KondisiRuang.java
@@ -0,0 +1,58 @@
+// di buat oleh : Khopsawati khotimah 
+
+package PreSistem;
+
+
+public class KondisiRuang {
+    
+private int Panjang;
+private int Lebar;
+private int jumlahkursi;
+private int jumlahpintu;
+private int jumlahjendela;
+
+    public int getPanjang() {
+        return Panjang;
+    }
+
+    public void setPanjang(int Panjang) {
+        this.Panjang = Panjang;
+    }
+
+    public int getLebar() {
+        return Lebar;
+    }
+
+    public void setLebar(int Lebar) {
+        this.Lebar = Lebar;
+    }
+
+    public int getJumlahkursi() {
+        return jumlahkursi;
+    }
+
+    public void setJumlahkursi(int jumlahkursi) {
+        this.jumlahkursi = jumlahkursi;
+    }
+
+    public int getJumlahpintu() {
+        return jumlahpintu;
+    }
+
+    public void setJumlahpintu(int jumlahpintu) {
+        this.jumlahpintu = jumlahpintu;
+    }
+
+    public int getJumlahjendela() {
+        return jumlahjendela;
+    }
+
+    public void setJumlahjendela(int jumlahjendela) {
+        this.jumlahjendela = jumlahjendela;
+    }
+
+
+
+
+    
+}
