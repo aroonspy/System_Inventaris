ad14f6d9126f0b5b71f59be89d40e861cfb993c2
Basri <aroonzfi@gmail.com>
2015-06-07 08:45:47 +0100

    System Inventaris Menggunakan NetBeans by Basri Basren

--- src/PreSistem/LogIn.java
+++ src/PreSistem/LogIn.java
@@ -1,92 +1,41 @@
 package PreSistem;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class LogIn {
-private String A;
-private String B;
-private String Nama;
-private String PasswordL;
-private String PasswordB;
-private String Nama2;
-private String Password2;
-private int Pilih;
-private int a = 1;
-Scanner input = new Scanner(System.in);
-
-private String SetNama (){
-	System.out.print("Masukkan Nama : ");
-	Nama = input.nextLine();
-	return Nama;
-}
-private String SetPasswordL (){
-	System.out.print("Masukkan Password : ");
-	PasswordL  = input.nextLine();
-	return PasswordL;
-}
-public int Menu(){
-	A = "1. GANTI PASSWORD";
-	B = "2. LOG IN";
-			
-	System.out.println(" ");
-	System.out.println("+++ Masukkan Pilihan +++");
-	System.out.println(A);
-	System.out.println(B);
-	System.out.print("Masukkan Pilihan : ");
-	Pilih = input.nextInt();
-	
-	return Pilih;
-
-}
-
-private String Gantipassword() {
-		System.out.println(" ");
-		System.out.println(A);
-		SetNama();
-		SetPasswordL();
-		System.out.print("Masukkan Password Baru: ");
-		PasswordB = input.nextLine();
-		
-		PasswordL=PasswordB;
-		return PasswordL;
-	}
-
-public void Masuk(String Nama,String PasswordL) {
-		System.out.println(" ");
-		System.out.println("LOG IN");
-		System.out.print("Masukkan Nama : ");
-		Nama2 = input.next();
-		System.out.print("Masukkan Password : ");
-		Password2  = input.next();
-		
-		if (Nama2.equals(Nama) && Password2.equals(PasswordL))
-			System.out.print("Log In Berhasil \n\n");
-		else System.out.print("Log In Gagal");
-	}
-public void Option(int Pilih, String Nama, String PasswordL){
-	if (Pilih == 1){
-		Gantipassword();
-		a = 2;
-		Logintest();
-	}
-	else if (Pilih == 2){	
-		Masuk(Nama,PasswordL);
-                
-	}
-	else
-		System.out.println("ERROR");
-}
-
-public void Logintest() {
-	if( a == 1){
-		SetNama();
-		SetPasswordL();
-		Menu();
-		Option(Pilih,Nama,PasswordL);
-	}
-	else if (a == 2){
-		Menu();
-		Option(Pilih,Nama,PasswordL);
-	}
+public String NamaCreat;
+public String PasswordCreat;
+public String Passwordconf;
+public String NamaLogin;
+public String PasswordLogin;
+public boolean Next;
+    
+    public void SetMater(String Nama, String Password, String ConfPass){
+        Next = false;
+        this.NamaCreat = Nama;
+        this.PasswordCreat = Password;
+        this.Passwordconf = ConfPass;
+        try{
+            if(this.PasswordCreat.equals(Passwordconf)){
+               Next = true;
+            }
+        }catch(Exception e){
+            System.out.println("ERROR");
+        }
     }
+    
+    public void SetLogin(String Nama, String Password) {
+        Next = false;
+        this.NamaLogin = Nama;
+        this.PasswordLogin = Password;
+        try{
+            if(this.NamaLogin.equals(NamaCreat) && this.PasswordLogin.equals(PasswordCreat)){
+                Next = true;
+                JOptionPane.showMessageDialog(null, "Anda Berhasil Login");
+            }
+        }catch(Exception e){
+            System.out.println("ERROR");
+        }
+    }
+    
 }
